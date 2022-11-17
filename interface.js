bd85782911df06e2e6f17d7304c3bc21d0291b4e
Kiko <kristijanivanovski2004@gmail.com>
2022-11-17 22:23:22 +0100

    added finished todos

--- interface.js
+++ interface.js
@@ -2,16 +2,27 @@
 const input = document.getElementById('addInput');
 // const input1 = document.getElementById('addInput1') as HTMLInputElement
 const addBtn = document.getElementById('addBtn');
-const list = document.getElementById('list');
+let list = document.getElementById('list');
+let finishedList = document.getElementById('finishedList');
 const restoreBtn = document.getElementById('restoreBtn');
 let todos = [];
-let finishedTodos = [];
+let finishedTodos = [
+    { name: 'Become succesful', id: 0, date: new Date() },
+    { name: 'Buy Milk', id: 1, date: new Date() },
+    { name: 'Take the laundry', id: 2, date: new Date() }
+];
+var editEl;
 var deletedEl;
 function loadTodos() {
     let newTodos = localStorage.getItem('todos');
+    let newFinishedTodos = localStorage.getItem('finishedTodos');
     if (newTodos) {
         todos = JSON.parse(newTodos);
         redrawList();
+    }
+    if (newFinishedTodos) {
+        finishedTodos = JSON.parse(newFinishedTodos);
+        drawFinished();
     }
 }
 window.addEventListener('load', loadTodos);
@@ -28,6 +39,15 @@ function nameCheck(inputName) {
 function fixId(array) {
     array.map((el) => (el.id = array.map((ele) => ele.name).indexOf(el.name)));
 }
+function drawFinished() {
+    finishedList.innerHTML = '';
+    finishedTodos.forEach((ele) => {
+        let li = document.createElement('span');
+        li.innerText = ele.name;
+        finishedList.append(li);
+    });
+}
+drawFinished();
 //Function for redrawing the todo list using the "todos" array.
 function redrawList() {
     list.innerHTML = '';
@@ -35,15 +55,20 @@ function redrawList() {
         let li = document.createElement('li');
         let deleteBtn = document.createElement('button');
         let finishBtn = document.createElement('button');
+        let editBtn = document.createElement('button');
+        // ------------------------------------------------------------
         finishBtn.setAttribute('class', 'finishBtn');
         finishBtn.addEventListener('click', () => {
             todos.splice(el.id, 1);
             let finishedEl = el;
             finishedTodos.push(finishedEl);
+            drawFinished();
             redrawList();
             fixId(todos);
             localStorage.setItem('todos', JSON.stringify(todos));
+            localStorage.setItem('finishedTodos', JSON.stringify(finishedTodos));
         });
+        // ---------------------------------------------------------------
         deleteBtn.setAttribute('class', 'deleteBtn');
         deleteBtn.addEventListener('click', () => {
             todos.splice(el.id, 1);
@@ -53,6 +78,14 @@ function redrawList() {
             localStorage.setItem('todos', JSON.stringify(todos));
             restoreBtn.disabled = false;
         });
+        // editBtn.setAttribute('class', 'editBtn')
+        // editBtn.innerText = 'a'
+        // editBtn.addEventListener('click', (): void => {
+        //   editEl = el
+        //   todos.splice(el.id, 1)
+        //   fixId(todos)
+        //   redrawList()
+        // })
         li.innerText = `${el.name}`;
         let liDiv = document.createElement('div');
         let checkImg = document.createElement('img');
@@ -62,8 +95,9 @@ function redrawList() {
         finishBtn.append(checkImg);
         deleteBtn.append(deleteImg);
         li.append(liDiv);
-        liDiv.append(deleteBtn, finishBtn);
+        liDiv.append(deleteBtn, finishBtn, editBtn);
         list.append(li);
+        li.setAttribute('class', 'toDoLi');
     });
     console.log(todos);
 }
@@ -77,21 +111,24 @@ restoreBtn.addEventListener('click', (e) => {
 addBtn.addEventListener('click', (e) => {
     // let num: number = parseInt(input1.value)
     e.preventDefault();
-    let nam = input.value;
-    let date = new Date();
-    let user1 = {
-        name: nam,
-        id: todos.length,
-        date: date
-    };
-    let item = todos.find((el) => el.name === nam);
-    if (item) {
-        console.log('go ima');
+    if (input.value !== '') {
+        let nam = input.value;
+        let date = new Date();
+        let user1 = {
+            name: nam,
+            id: todos.length,
+            date: date
+        };
+        let item = todos.find((el) => el.name === nam);
+        if (item) {
+            console.log('go ima');
+        }
+        else {
+            todos.push(user1);
+            localStorage.setItem('todos', JSON.stringify(todos));
+            redrawList();
+            input.value = '';
+        }
     }
-    else {
-        todos.push(user1);
-        localStorage.setItem('todos', JSON.stringify(todos));
-        redrawList();
-        input.value = '';
-    }
+    fixId(todos);
 });
--- interface.ts
+++ interface.ts
@@ -1,7 +1,8 @@
 const input = document.getElementById('addInput') as HTMLInputElement
 // const input1 = document.getElementById('addInput1') as HTMLInputElement
 const addBtn = document.getElementById('addBtn') as HTMLButtonElement
-const list = document.getElementById('list') as HTMLOListElement
+let list = document.getElementById('list') as HTMLOListElement
+let finishedList = document.getElementById('finishedList') as HTMLOListElement
 const restoreBtn = document.getElementById('restoreBtn') as HTMLButtonElement
 
 type User = {
@@ -10,15 +11,24 @@ type User = {
   date: Date
 }
 let todos: User[] = []
-let finishedTodos: User[] = []
-
+let finishedTodos: User[] = [
+  { name: 'Become succesful', id: 0, date: new Date() },
+  { name: 'Buy Milk', id: 1, date: new Date() },
+  { name: 'Take the laundry', id: 2, date: new Date() }
+]
+var editEl: User
 var deletedEl: User
 
 function loadTodos() {
   let newTodos = localStorage.getItem('todos')
+  let newFinishedTodos = localStorage.getItem('finishedTodos')
   if (newTodos) {
     todos = JSON.parse(newTodos)
     redrawList()
+  }
+  if (newFinishedTodos) {
+    finishedTodos = JSON.parse(newFinishedTodos)
+    drawFinished()
   }
 }
 window.addEventListener('load', loadTodos)
@@ -36,26 +46,39 @@ function nameCheck(inputName: string) {
 function fixId(array: User[]): void {
   array.map((el) => (el.id = array.map((ele) => ele.name).indexOf(el.name)))
 }
+function drawFinished(): void {
+  finishedList.innerHTML = ''
+  finishedTodos.forEach((ele) => {
+    let li = document.createElement('span')
+    li.innerText = ele.name
+    finishedList.append(li)
+  })
+}
+drawFinished()
 
 //Function for redrawing the todo list using the "todos" array.
 function redrawList() {
   list.innerHTML = ''
+
   todos.forEach((el) => {
     let li = document.createElement('li')
     let deleteBtn = document.createElement('button')
     let finishBtn = document.createElement('button')
+    let editBtn = document.createElement('button')
+    // ------------------------------------------------------------
     finishBtn.setAttribute('class', 'finishBtn')
-
     finishBtn.addEventListener('click', (): void => {
       todos.splice(el.id, 1)
       let finishedEl: User = el
       finishedTodos.push(finishedEl)
+      drawFinished()
       redrawList()
       fixId(todos)
       localStorage.setItem('todos', JSON.stringify(todos))
+      localStorage.setItem('finishedTodos', JSON.stringify(finishedTodos))
     })
+    // ---------------------------------------------------------------
     deleteBtn.setAttribute('class', 'deleteBtn')
-
     deleteBtn.addEventListener('click', (): void => {
       todos.splice(el.id, 1)
       deletedEl = el
@@ -64,6 +87,14 @@ function redrawList() {
       localStorage.setItem('todos', JSON.stringify(todos))
       restoreBtn.disabled = false
     })
+    // editBtn.setAttribute('class', 'editBtn')
+    // editBtn.innerText = 'a'
+    // editBtn.addEventListener('click', (): void => {
+    //   editEl = el
+    //   todos.splice(el.id, 1)
+    //   fixId(todos)
+    //   redrawList()
+    // })
     li.innerText = `${el.name}`
     let liDiv = document.createElement('div')
     let checkImg = document.createElement('img')
@@ -73,8 +104,9 @@ function redrawList() {
     finishBtn.append(checkImg)
     deleteBtn.append(deleteImg)
     li.append(liDiv)
-    liDiv.append(deleteBtn, finishBtn)
+    liDiv.append(deleteBtn, finishBtn, editBtn)
     list.append(li)
+    li.setAttribute('class', 'toDoLi')
   })
   console.log(todos)
 }
@@ -89,20 +121,24 @@ restoreBtn.addEventListener('click', (e): void => {
 addBtn.addEventListener('click', (e): void => {
   // let num: number = parseInt(input1.value)
   e.preventDefault()
-  let nam: string = input.value
-  let date = new Date()
-  let user1: User = {
-    name: nam,
-    id: todos.length,
-    date: date
+  if (input.value !== '') {
+    let nam: string = input.value
+    let date = new Date()
+    let user1: User = {
+      name: nam,
+      id: todos.length,
+      date: date
+    }
+
+    let item = todos.find((el) => el.name === nam)
+    if (item) {
+      console.log('go ima')
+    } else {
+      todos.push(user1)
+      localStorage.setItem('todos', JSON.stringify(todos))
+      redrawList()
+      input.value = ''
+    }
   }
-  let item = todos.find((el) => el.name === nam)
-  if (item) {
-    console.log('go ima')
-  } else {
-    todos.push(user1)
-    localStorage.setItem('todos', JSON.stringify(todos))
-    redrawList()
-    input.value = ''
-  }
+  fixId(todos)
 })
